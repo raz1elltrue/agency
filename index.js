b2d209b0334359e3703d730a3704800772082488
Andrii Kulyzhko <akulyzhko@gmail.com>
2019-03-24 12:37:16 +0200

    initial 24/03/2019

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.render('index', {data: data});
 });
 
+app.get('/create', function (req, res) {
+  res.render('create');
+});
+
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
 });
