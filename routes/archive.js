15278f8f284ddac3a7053a64817a716cfecb7438
Andrii Kulyzhko <akulyzhko@gmail.com>
2019-05-12 00:06:09 +0300

    add links to writer

--- routes/archive.js
+++ routes/archive.js
@@ -4,35 +4,59 @@ const router = express.Router();
 const config = require("../config");
 const models = require("../models");
 
-function posts(req, res) {
+async function posts(req, res) {
   const userId = req.session.userId;
   const userLogin = req.session.userLogin;
   const perPage = +config.PER_PAGE;
   const page = req.params.page || 1;
 
-  models.Post.find({})
-    .skip(perPage * page - perPage)
-    .limit(perPage)
-    .then(posts => {
-      models.Post.countDocuments()
-        .then(countDocuments => {
-          res.render("index", {
-            posts,
-            current: page,
-            pages: Math.ceil(countDocuments / perPage),
-            user: {
-              id: userId,
-              login: userLogin
-            }
-          });
-        })
-        .catch(() => {
-          throw new Error("Server Error");
-        });
-    })
-    .catch(() => {
-      throw new Error("Server Error");
+  try {
+    const posts = await models.Post.find({})
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .populate("owner")
+      .sort({ createdAt: -1 });
+
+    const count = await models.Post.countDocuments();
+
+    res.render("archive/index", {
+      posts,
+      current: page,
+      pages: Math.ceil(count / perPage),
+      user: {
+        id: userId,
+        login: userLogin
+      }
     });
+  } catch (error) {
+    throw new Error("Server Error");
+  }
+
+  //   models.Post.find({})
+  //     .skip(perPage * page - perPage)
+  //     .limit(perPage)
+  //     .populate("owner")
+  //     .sort({ createdAt: -1 })
+  //     .then(posts => {
+  //       models.Post.countDocuments()
+  //         .then(countDocuments => {
+  //           res.render("archive/index", {
+  //             posts,
+  //             current: page,
+  //             pages: Math.ceil(countDocuments / perPage),
+  //             user: {
+  //               id: userId,
+  //               login: userLogin
+  //             }
+  //           });
+  //         })
+  //         .catch(() => {
+  //           throw new Error("Server Error");
+  //         });
+  //     })
+  //     .catch(() => {
+  //       throw new Error("Server Error");
+  //     });
 }
 
 // routers
@@ -66,6 +90,49 @@ router.get("/posts/:post", (req, res, next) => {
       }
     });
   }
+});
+
+// users posts
+router.get("/users/:login/:page*?", (req, res) => {
+  const userId = req.session.userId;
+  const userLogin = req.session.userLogin;
+  const perPage = +config.PER_PAGE;
+  const page = req.params.page || 1;
+  const login = req.params.login;
+
+  models.User.findOne({
+    login
+  }).then(user => {
+    models.Post.find({
+      owner: user.id
+    })
+      .skip(perPage * page - perPage)
+      .limit(perPage)
+      .sort({ createdAt: -1 })
+      .then(posts => {
+        models.Post.countDocuments({
+          owner: user.id
+        })
+          .then(countDocuments => {
+            res.render("archive/user", {
+              posts,
+              _user: user,
+              current: page,
+              pages: Math.ceil(countDocuments / perPage),
+              user: {
+                id: userId,
+                login: userLogin
+              }
+            });
+          })
+          .catch(() => {
+            throw new Error("Server Error");
+          });
+      })
+      .catch(() => {
+        throw new Error("Server Error");
+      });
+  });
 });
 
 module.exports = router;
